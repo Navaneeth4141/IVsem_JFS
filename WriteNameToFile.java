da4e0708375ce971ebc2dd18cecb6ceff38e0e6b
Navaneeth4141 <tnavaneethreddy07@gmail.com>
2026-02-20 10:30:29 +0530

    Java program output to File

--- WriteNameToFile.java
+++ WriteNameToFile.java
@@ -1,11 +1,13 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class WriteNameToFile {
     public static void main(String[] args) {
         try {
-            FileWriter writer = new FileWriter("name.txt");
-            writer.write("Hello Navaneeth");
+            Scanner sc = new Scanner(System.in);
+            FileWriter writer = new FileWriter("name.txt",true);
+            writer.write(sc.nextLine());
             writer.close();
             System.out.println("Successfully wrote to the file");
         } catch (IOException e) {
