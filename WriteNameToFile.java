da0f1385d0e1c3b966c5d58728d41e5d50348eb0
Navaneeth4141 <tnavaneethreddy07@gmail.com>
2026-02-20 10:27:11 +0530

    Name to File

--- WriteNameToFile.java
+++ WriteNameToFile.java
@@ -5,7 +5,7 @@ public class WriteNameToFile {
     public static void main(String[] args) {
         try {
             FileWriter writer = new FileWriter("name.txt");
-            writer.write("Ashok");
+            writer.write("Navaneeth");
             writer.close();
             System.out.println("Successfully wrote to the file");
         } catch (IOException e) {
