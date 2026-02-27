2701c08adc561d9f23e5852dad6bd8ccaeb88b00
Navaneeth4141 <tnavaneethreddy07@gmail.com>
2026-02-27 11:01:15 +0530

    Reading data from a file

--- ReadNameFromFile.java
+++ ReadNameFromFile.java
@@ -1,24 +1,22 @@
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
-
-public class ReadNameFromFile {
-    public static void main(String[] args) {
-        try {
-            System.out.print("Enter the filename : ");
-            Scanner sc = new Scanner(System.in);
-            String filename = sc.nextLine();
-            File file = new File(filename);
-            Scanner sc1 = new Scanner(file);
-            while(sc1.hasNextLine()){
-                String s = sc1.nextLine();
-                System.out.println(s);
-            }
-            sc.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred");
-            e.printStackTrace();
-        }
-    }
+public class ReadNameToFile{
+	public static void main(String args[]){
+		try{
+			File file=new File("student.txt");
+			Scanner sc=new Scanner(file);
+			while(sc.hasNextLine()){
+				String name=sc.nextLine();
+				System.out.println(name);	
+			}
+			sc.close();
+			System.out.print("Name Read Successfully.");
+		}
+		catch(IOException e){
+			System.out.println("File Not Found.");
+			e.printStackTrace();
+		}
+	}
 }
